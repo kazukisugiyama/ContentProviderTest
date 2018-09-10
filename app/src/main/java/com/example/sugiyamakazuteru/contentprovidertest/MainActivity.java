f3072037f1ade4ee33d4e6518bb3165536dda3d8
kazukisugiyama <s.kgmgmjtd579@gmail.com>
2018-09-10 11:45:49 +0900

    ContentProviderで画面の明るさを取得

--- app/src/main/java/com/example/sugiyamakazuteru/contentprovidertest/MainActivity.java
+++ app/src/main/java/com/example/sugiyamakazuteru/contentprovidertest/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.sugiyamakazuteru.contentprovidertest;
 
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        TextView text = (TextView) findViewById(R.id.textView);
+        String btext = Settings.System.getString
+                (this.getContentResolver(), "screen_brightness");
+
+        text.setText("画面の明るさは、" + btext);
     }
 }
