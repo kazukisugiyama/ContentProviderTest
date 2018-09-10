0dccaf5318d67543a16d23da6798a256271df2f2
kazukisugiyama <s.kgmgmjtd579@gmail.com>
2018-09-10 18:28:08 +0900

    contentprovider作成 ビルドが通らないので修正中

--- app/src/main/java/com/example/sugiyamakazuteru/contentprovidertest/MainActivity.java
+++ app/src/main/java/com/example/sugiyamakazuteru/contentprovidertest/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.sugiyamakazuteru.contentprovidertest;
 
-import android.provider.Settings;
+
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,8 +14,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // コンテンツプロバイダURIを定義
-        
+        String[] projection = new String[]
+                {TestContentProvider.Columns.NAME};
+        Cursor cursor = getContentResolver().query(TestContentProvider.testUri,
+                projection, null,null);
+
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+                android.R.layout.simple_list_item_1, cursor,
+                new String[]{TestContentProvider.Columns.NAME},
+                new int[] {android.R.id.text1},
+                0);
+
+        TextView texitview = findViewById(R.id.textView);
+        texitview.setText((CharSequence) adapter);
 
 
 /*        TextView text = (TextView) findViewById(R.id.textView);
--- app/src/main/java/com/example/sugiyamakazuteru/contentprovidertest/TestDB.java
+++ app/src/main/java/com/example/sugiyamakazuteru/contentprovidertest/TestDB.java
@@ -8,17 +8,17 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class TestDB extends SQLiteOpenHelper {
 
     // DB、テーブル情報定義
-    private final String DB_NAME = "PohoneBookDB.db";
-    private final int DB_VERSION = 1;
+    private static final String DB_NAME = "PohoneBookDB.db";
+    private static final int DB_VERSION = 1;
     public final String DB_TABLE_NAME = "PhoneBookTable";
     public final String COL_NAME = "PhoneNumber";
 
     // テーブル作成処理
     private final String STRING_CREATE = "CREATE TABLE " + DB_TABLE_NAME +
-            "(INTEGER PRIMARY KEY AUTOINCREMENT, " + COL_NAME + ");";
+            "(_id INTEGER PRIMARY KEY AUTOINCREMENT, " + COL_NAME + ");";
 
-    public TestDB(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public TestDB(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
     }
 
     @Override
@@ -39,7 +39,7 @@ public class TestDB extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        // DB変更処理時にドロップする　暫定
+        // DB変更処理時にドロップする　暫定　バージョンが新しくなった時の処理を後ほど追加
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + DB_TABLE_NAME);
     }
 }
